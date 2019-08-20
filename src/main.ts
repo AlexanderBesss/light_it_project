d22922a369f3c3c5237865e84c34ee61efc13043
Alexander Besarab <alexandrbesss@gmail.com>
2019-08-20 18:01:41 +0300

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  const baseUrlPath = 'api';
+  app.setGlobalPrefix(baseUrlPath);
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Docs')
+    .setDescription('The docs of API')
+    .setVersion('1.0')
+    .addTag('users')
+    .setBasePath(baseUrlPath)
+    // .addBearerAuth('Authorization', 'header')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
